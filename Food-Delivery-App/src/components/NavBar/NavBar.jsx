8f8d62078d22300465cb6c07647569f41672d92e
Valeria Mikhalevich <valeerriam@gmail.com>
2025-03-30 16:43:19 +0300

    add: complete navbar

--- Food-Delivery-App/src/components/NavBar/NavBar.jsx
+++ Food-Delivery-App/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { assets } from "../../assets/assets";
 
 const NavBar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
-      <img src={assets.logo}></img>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          главная
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          меню
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          приложение
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          контакты
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>войти</button>
+      </div>
     </div>
   );
 };
