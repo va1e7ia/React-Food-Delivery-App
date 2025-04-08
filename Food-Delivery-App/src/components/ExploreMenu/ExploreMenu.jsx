d460f7602f4dba2d594505948f25371f09393b8a
Valeria Mikhalevich <valeerriam@gmail.com>
2025-04-09 00:30:20 +0300

    add: explore menu jsx + css

---
+++ Food-Delivery-App/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = () => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Взгляните на меню</h1>
+      <p className="explore-menu-text">
+        Выбирайте из разнообразного меню, где вас ждёт изысканное многообразие
+        блюд, приготовленных из лучших ингредиентов с мастерским кулинарным
+        искусством. Наша миссия — радовать ваш вкус и превращать каждый приём
+        пищи в настоящее наслаждение.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
--- Food-Delivery-App/src/pages/Home/Home.jsx
+++ Food-Delivery-App/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import Header from "../../components/NavBar/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   );
 };
