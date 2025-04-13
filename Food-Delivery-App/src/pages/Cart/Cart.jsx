437b992d1631c5852f0d60b854560d6941327c36
Valeria Mikhalevich <valeerriam@gmail.com>
2025-04-13 16:47:50 +0300

    add: start creating cart page

--- Food-Delivery-App/src/pages/Cart/Cart.jsx
+++ Food-Delivery-App/src/pages/Cart/Cart.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Блюдо</p>
+          <p>Название</p>
+          <p>Цена</p>
+          <p>Количество</p>
+          <p>Итого</p>
+          <p>Удалить</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price} BYN</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]} BYN</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
