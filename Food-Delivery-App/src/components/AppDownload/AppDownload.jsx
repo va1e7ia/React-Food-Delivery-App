a4fec76857c75e6284ca3f01570efda98840b7ec
Valeria Mikhalevich <valeerriam@gmail.com>
2025-04-13 15:11:57 +0300

    add: app download cpmponent

---
+++ Food-Delivery-App/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        Для более комфортного использования скачивайте приложение
+        <br />
+        Доставка вкуса
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
